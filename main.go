dfda7757c95380aeadb97e87b685be844688f498
vintweb <vintweb@gmail.com>
2025-01-03 09:10:44 +0500

    Добавление методов PATCH & DELETE

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -27,7 +28,7 @@ func GetMessage(w http.ResponseWriter, r *http.Request) {
 func CreateMessage(w http.ResponseWriter, r *http.Request) {
 	var message Message
 
-	// Декодируем JSON из тела запроса в структуру Messega
+	// Декодируем JSON из тела запроса в структуру Message
 	if err := json.NewDecoder(r.Body).Decode(&message); err != nil {
 		http.Error(w, "Неверный формат JSON", http.StatusBadRequest)
 		return
@@ -45,6 +46,61 @@ func CreateMessage(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(message)
 }
 
+// PATHC: Обновление задачи
+func UpdateMessage(w http.ResponseWriter, r *http.Request) {
+	// Получение ID из параметров
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Не корректный ID", http.StatusBadRequest)
+		return
+	}
+
+	//Поиск задачи по ID
+	var message Message
+	if result := DB.First(&message, id); result.Error != nil {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+
+	// Декодируем JSON из запроса
+	var updates map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&updates); err != nil {
+		http.Error(w, "Неверый формат JSON", http.StatusBadRequest)
+		return
+	}
+
+	// Обновляем запись
+	if result := DB.Model(&message).Updates(updates); result.Error != nil {
+		http.Error(w, "Ошибка обновления задачи", http.StatusInternalServerError)
+		return
+	}
+
+	// Возвращаем обновленную задачу
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
+// DELETE: Удаление задачи
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	// Получение ID из каталога
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Некорректный ID", http.StatusBadRequest)
+		return
+	}
+
+	// DELETE: Удаление задачи по ID
+	if result := DB.Delete(&Message{}, id); result.Error != nil {
+		http.Error(w, "Ошибка удаления задачи", http.StatusInternalServerError)
+		return
+	}
+
+	// Возвращаем успешный статус
+	w.WriteHeader(http.StatusNoContent) // HTTP 204 No Content
+}
+
 func main() {
 	// вызываем метод InitDB() из файла db.go
 	InitDB()
@@ -56,6 +112,8 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessage).Methods("GET")
+	router.HandleFunc("/api/messages/{id}", UpdateMessage).Methods("PATCH")
+	router.HandleFunc("/api/messages/{id}", DeleteMessage).Methods("DELETE")
 
 	// Запуск сервера
 	fmt.Println("The server is running on port 8080...")
